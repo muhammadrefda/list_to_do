bf5b3cbdfef71bb4b7f7d21339a27aa7e28b1031
hfdzafrnsyh <afrnsyhfdz@gmail.com>
2020-09-01 00:43:29 +0700

    route group auth

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::group(['middleware' => ['auth']] ,function(){
 Route::get('/home', 'HomeController@index')->name('home');
 
 // category
 Route::get('category' , 'Category\WebCategoryController@index');
 Route::get('category/{id}/edit' , 'Category\WebCategoryController@edit');
+});
