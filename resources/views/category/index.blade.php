4b74fcb8e7685343721c5c8afbc6711f351c58a5
hfdzafrnsyh <afrnsyhfdz@gmail.com>
2020-09-01 04:33:13 +0700

    get table category di index

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -2,6 +2,49 @@
 @section('title' , 'Category')
 @section('wp-content')
 
-    <h1>ini category</h1>
+    <div class="content">
+    <h3>Category</h3>
+
+
+    <div class="data-table">
+    
+    <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Category_Id</th>
+      <th scope="col">name</th>
+      <th scope="col">Action</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+   @foreach($categories as $category)
+    <tr>
+      <th scope="row">{{ $loop->iteration }}</th>
+      <td>{{ $category->id}}</td>
+      <td>{{ $category->name}}</td>
+      <td>
+      <a href="#" class="btn btn-info btn-circle btn-sm">
+                    <i class="fas fa-info"></i>
+                  </a>
+                <a href="#" class="btn btn-warning btn-circle btn-sm">
+                    <i class="fas fa-edit"></i>
+                  </a>
+                  <form action="#" method="post" class="d-inline">
+                     @method('delete')
+                     @csrf
+                      <button type="submit" class="btn btn-danger btn-circle btn-sm justify-content-end"><i class="fas fa-trash "></i></button>
+                    </form>
+      </td>
+    </tr>
+   
+   @endforeach
+  </tbody>
+</table>
+
+    </div>
+
+    </div>
 
 @endsection
